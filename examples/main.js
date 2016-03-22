0dcd49f314676fbf0c7fc912a0dda9d6bc2ec095
Steve Kane <kanesteven@gmail.com>
2016-03-22 13:23:33 -0500

    inital commit with reasonable working example

---
+++ examples/main.js
@@ -0,0 +1,25 @@
+var KeyboardSignal = require('../index.js')
+var kbs = new KeyboardSignal(document.body)
+var E_EL = document.getElementById('E')
+var KC_EL = document.getElementById('69')
+
+function makeUpdate () {
+  var last = Date.now()
+  var current = Date.now()
+  var dT = current - last
+
+  return function update (dT) {
+    last = current
+    current = Date.now()
+    dT = current - last
+
+    KeyboardSignal.update(dT, kbs)
+
+    E_EL.innerText = 'E: ' + JSON.stringify(kbs.E, null, 2)
+    KC_EL.innerText = '69: ' + JSON.stringify(kbs[69], null, 2)
+    requestAnimationFrame(update)
+  }
+}
+
+window.kbs = kbs
+requestAnimationFrame(makeUpdate())
---
+++ src/index.js
@@ -0,0 +1,85 @@
+var CONSTANTS = require('./CONSTANTS')
+var KEY_COUNT = CONSTANTS.KEY_COUNT
+var BASE_KEY_MAP = CONSTANTS.BASE_KEY_MAP
+
+function Enum () {
+  var values = Array.prototype.slice.call(arguments, 0)
+  var en = {}
+
+  for (var i = 0, v; v = values[i++];) {
+    en[v] = v
+  }
+  return en
+}
+
+var KEY_MODE = Enum('INACTIVE', 'DOWN', 'UP', 'JUST_DOWN', 'JUST_UP')
+
+module.exports = KeyboardSignal
+
+function KeyState () {
+  this.previousMode = KEY_MODE.UP
+  this.mode = KEY_MODE.UP
+  this.downDuration = 0
+}
+
+function KeyboardSignal (body, keyMap) {
+  keyMap = keyMap || BASE_KEY_MAP
+  var self = this
+
+  for (var i = 0; i < KEY_COUNT; i++) {
+    this[i] = new KeyState
+  }
+
+  for (var key in keyMap) defProp(key)
+
+  function defProp (key) {
+    Object.defineProperty(self, key, {
+      enumerable: false,
+      get: function () { 
+        var keyCode = keyMap[key]
+        var ks = self[keyCode]
+
+        return ks
+      }
+    }) 
+  }
+
+  function keydown (e) {
+    var ks = self[e.keyCode]
+
+    if (ks.mode !== KEY_MODE.DOWN) ks.mode = KEY_MODE.JUST_DOWN
+  }
+
+  function keyup (e) {
+    var ks = self[e.keyCode]
+
+    ks.mode = KEY_MODE.JUST_UP
+  }
+
+  function blur () {
+    for (var i = 0, ks; ks = self[i]; i++) {
+      ks.mode = KEY_MODE.INACTIVE
+    }
+  }
+
+  function focus () {
+    for (var i = 0, ks; ks = self[i]; i++) {
+      ks.mode = KEY_MODE.UP
+    }
+  }
+
+  body.addEventListener('keydown', keydown)
+  body.addEventListener('keyup', keyup)
+  body.addEventListener('blur', blur)
+  body.addEventListener('focus', focus)
+}
+
+KeyboardSignal.update = function (dT, kbs) {
+  for (var i = 0, ks; ks = kbs[i]; i++) {
+    if      (ks.previousMode === KEY_MODE.JUST_DOWN) ks.mode = KEY_MODE.DOWN
+    else if (ks.previousMode === KEY_MODE.JUST_UP)   ks.mode = KEY_MODE.UP
+    else {}
+    ks.downDuration = ks.mode === KEY_MODE.DOWN ? ks.downDuration + dT : 0
+    ks.previousMode = ks.mode
+  }
+}
